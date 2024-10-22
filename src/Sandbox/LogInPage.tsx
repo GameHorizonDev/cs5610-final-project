27263332a3e0401ccdbae68decc706a811690d20
Yanling Peng <108501928+francyorange@users.noreply.github.com>
2024-10-21 22:40:16 -0700

    import Bootstrap and add demo page (#6)

---
+++ src/Sandbox/LogInPage.tsx
@@ -0,0 +1,12 @@
+import LoginPage from '../Pages/LogInPage';
+
+export default function SandboxLoginPage() {
+    return (
+        <div>
+            <h1>GameHorizon</h1>
+            <LoginPage />
+            <h2>Content List</h2>
+            <h2>Footer</h2>
+        </div>
+    )
+}
