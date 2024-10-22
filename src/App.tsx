673b096eaf02b1d86d7d1e0affe31bf70b652b31
Yanling Peng <108501928+francyorange@users.noreply.github.com>
2024-10-22 04:14:16 +0000

    clean up on landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Example from './API/Example'
 import LoginPage from './Pages/LogInPage';
@@ -10,20 +9,6 @@ import ProfileEditPage from './Pages/ProfileEditPage';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Router>
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
@@ -31,10 +16,10 @@ function App() {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/profile/edit" element={<ProfileEditPage />} />
+          <Route path="/sandbox/api/example" element={<Example />} />
         </Routes>
       </Router>
 
-      <Example />
     </div>
   );
 }
