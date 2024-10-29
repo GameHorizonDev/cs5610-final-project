6581319d0eb37957184ebe7443c288ee5a0f1d6e
Yanling Peng <108501928+francyorange@users.noreply.github.com>
2024-10-28 21:10:18 -0700

    Create GameReviews Prototype (#8)

--- src/Sandbox/index.tsx
+++ src/Sandbox/index.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router";
 import SandboxHomePage from "./HomePage";
 import SandboxLoginPage from "./LogInPage";
 import SandboxBootstrapDemo from "./BootstrapDemo";
+import SandboxGameReview from "./GameReview";
 export default function Sandbox() {
     return (
         <Routes>
             <Route path="/Homepage/*" element={<SandboxHomePage />} />
             <Route path="/Login/*" element={<SandboxLoginPage />} />
+            <Route path="/GameReview/*" element={<SandboxGameReview />} />
             <Route path="/BootstrapDemo/*" element={<SandboxBootstrapDemo />} />
 
         </Routes>
