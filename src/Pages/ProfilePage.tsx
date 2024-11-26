7648068f67b946a90e4559f8394f71fa5177b795
VivianDongChen <chen.dong1@northeastern.edu>
2024-11-26 14:15:30 -0800

    add BookmarkedReview Page to ProfilePage

--- src/Pages/ProfilePage.tsx
+++ src/Pages/ProfilePage.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { SERVER_BASE_URL, APP_AXIOS } from '../API/apiConfig';
 import { Link } from 'react-router-dom'; 
+import BookmarkedReviews from './BookmarkedReviews';
 
 const ProfilePage: React.FC = () => {
   const [username, setUsername] = useState('');
@@ -30,7 +31,8 @@ const ProfilePage: React.FC = () => {
   }
 
   return (
-    <div>
+    <div className="profile-container" style={{ display: 'flex', gap: '20px' }}>
+    <div style={{ flex: 1 }}>
       <h2>Profile</h2>
       <div>
         <strong>Username:</strong>
@@ -50,8 +52,12 @@ const ProfilePage: React.FC = () => {
       <Link to="/profile/edit">
         <button type="button">Manage Account</button>
       </Link>
-
     </div>
+
+    <div style={{ flex: 1 }}>
+        <BookmarkedReviews />
+      </div>
+       </div>
   );
 };
 
