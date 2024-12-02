4c98bfecb658f630e17e1c0bc4427151ca122b1c
VivianDongChen <chen.dong1@northeastern.edu>
2024-12-02 15:01:23 -0800

    check if the user is logged in, if not navigate to the login page

--- src/Pages/ProfilePage.tsx
+++ src/Pages/ProfilePage.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { SERVER_BASE_URL, APP_AXIOS } from "../API/apiConfig";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import BookmarkedReviews from "./BookmarkedReviews";
 import FavoritedGames from "./FavoritedGames";
 import "../ProfilePage.css";
+
 
 const ProfilePage: React.FC = () => {
   const [username, setUsername] = useState("");
@@ -11,6 +12,7 @@ const ProfilePage: React.FC = () => {
   const [role, setRole] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchProfile = async () => {
@@ -24,6 +26,7 @@ const ProfilePage: React.FC = () => {
       } catch (error) {
         console.error("Error fetching profile:", error);
         setError("Failed to load profile.");
+        navigate("/login"); 
       }
     };
 
